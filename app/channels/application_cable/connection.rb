5c7edea90ae151d583765754626eb92124463554
Edward Heaver <edward.heaver01@gmail.com>
2022-08-16 20:43:26 +0100

    rubocop and config

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,7 +1,5 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    def thing
-
-    end
+    def thing; end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +18,6 @@ module ProjectStarter
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.belongs_to_required_by_default = false
   end
 end
